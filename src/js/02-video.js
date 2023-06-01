a20f34e3b008123a54cfd16dc82aa4bb75e3224b
Makarenko Pavlo <cmertnik1986@gmail.com>
2023-06-01 23:42:40 +0300

    hw8 page-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+const saveCurrentTime = throttle(async function() {
+  const currentTime = await player.getCurrentTime();
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000);
+async function restorePlayback() {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    await player.setCurrentTime(currentTime);
+  }
+}
+player.on('timeupdate', saveCurrentTime);
+restorePlayback();
